468e06e72c2f71b77f0a3f1b790d99d12e8a1775
enniefu <enniefu@gmail.com>
2019-06-17 21:21:53 +0800

    使得训练数据只有正样本

--- oneclass手写识别.py
+++ oneclass手写识别.py
@@ -2,26 +2,34 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.svm import OneClassSVM
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 digits = datasets.load_digits()
 X=digits.data
 y=digits.target
 
+
+
+
 for aa in range(10):
+
+
     X_train, X_test, y_train_pre, y_test_pre = train_test_split(X, y, test_size=0.3)
     y_train=[]
     y_test=[]
 
+
+
     for i in y_train_pre:
         if i==aa:
-            y_train.append(i)
-        else:
-            y_train.append(-1)
+            y_train.append(1)
+        # else:
+        #     y_train.append(-1)
 
     for i in y_test_pre:
         if i==aa:
-            y_test.append(i)
+            y_test.append(1)
         else:
             y_test.append(-1)
 
@@ -34,9 +42,8 @@ for aa in range(10):
 
     ans=y_test-OCSVM.predict((X_test))
 
-    i
+    i=0
     for a in ans:
         if a==0:
             i=i+1
     print(aa,":   ",i/len(ans))
-
